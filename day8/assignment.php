63081587f84ebcef6f78a60e510178c35c49c09e
Chwen <zzero.i.n.g@gmail.com>
2024-03-04 12:09:57 +0630

    updated commit

--- day8/assignment.php
+++ day8/assignment.php
@@ -4,39 +4,30 @@ $conn = new mySqli("localhost", "root", "", "php_project");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
 // echo "Connected successfully";
 
-
 if ($_SERVER["REQUEST_METHOD"] === "POST") { 
+    
+    
 
+
+    if (isset($_POST["action"]) ?? "" === "DELETE") {
+        // echo $_POST['id']; die();
+        $id = $_POST["id"];
+        $delete_sql = "DELETE FROM blog WHERE id = $id;";
+        $conn->query($delete_sql);
+        // $result = $conn->query($delete_sql);
+    }else{
         $title = $_POST['title'];
         $body = $_POST['body'];
         $stmt = $conn->prepare("INSERT INTO blog (title, body) VALUES (?, ?)");
         $stmt->bind_param("ss", $title, $body);
         $stmt->execute();
-
-        // if ($conn->query($sql) === TRUE) { 
-        //     $last_id = $conn->insert_id;
-        //     $info = "New record created successfully. Last inserted ID is: ".$last_id;
-        // } else {
-        //     $info = "Error: ". $sql. "<br>". $conn->error;
-        // }
-
-
-    if ($_POST["action"] === "DELETE") {
-        // echo $_POST['id']; die();
-        $id = $_POST["id"];
-        $delete_sql = "DELETE FROM blog WHERE id = $id";
-        $conn->query($delete_sql);
-    } 
-    // if ($_POST["action"] === "DELETE") {
-    //     // echo $_POST['id']; die();
-    //     $id = $_POST["id"];
-    //     $delete_sql = "DELETE FROM blog WHERE id = $id";
-    //     $conn->query($delete_sql);
-    // } 
+    }
 }
+
+$sql = "SELECT * FROM blog ORDER BY id DESC ";
+$result = $conn->query($sql);
 ?>
 
 
@@ -52,12 +43,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     
     <div class="container">
         <?php if (isset($info)) : ?>
-            <div class="alert alert-info">
+            <div class="alert alert-info mt-3">
                 <?= $info ?>
             </div>
         <?php endif ?>
 
-        <form action="assignment.php" method="post" class="form w-50 m-auto">
+        <form action="assignment.php" method="post" class="form w-50 mx-auto mt-3">
             <h1 class="text-primary">Form</h1>
             <input type="text" name="title" class="form-control mb-2" placeholder="your title" required>
             <textarea name="body" id="" cols="30" rows="10" class="form-control" placeholder="type your text ..." required></textarea> <br>
@@ -65,29 +56,34 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             <button class="btn btn-primary w-25">Add</button>
         </form>
 
-        <div class="d-flex flex-wrap">
-            <?php 
-            $sql = "SELECT * FROM blog ORDER BY id DESC ";
-            $result = $conn->query($sql);
-            ?>
-            <?php if ($result->num_rows > 0) :  ?>
-                <!-- change result(Obj) to associative array -->
-            
-                <?php while ($row = $result->fetch_assoc()) : ?>
-                    <div class="card w-50">
-                        <div class="card-body">
-                            <h4 class="card-title">Title : <?= $row['title'] ?></h4>
-                            <p class="card-text">Body : <?= $row['body']?></p>
-                            <form action="assignment.php" method="POST">
-                                <input type="hidden" name="id" value="<?= $row['id'] ?>">
-                                <input type="hidden" name="action" value="DELETE">
-                                <input type="submit" class="btn btn-danger w-100" value="Delete">
-                            </form>
-                        </div>
+        <div>
+            <table class="table table-striped mt-5">
+                <tr>
+                    <th>NO</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                    <th>Action</th>
+                </tr>
+                <?php $i = 1 ?>
+                <?php while ($row = $result->fetch_assoc())  : ?>
+                    <div>
+                        <tr>
+                            <td><?= $i ?> </td>
+                            <td><?= htmlentities($row['title']) ?> </td>
+                            <td><?= htmlentities($row['body']) ?> </td>
+                            <td>
+                                <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" >
+                                    <input type="hidden" name="id" value=<?= $row['id'] ?>>
+                                    <input type="hidden" name="action" value="DELETE">
+    
+                                    <button type="submit" class="btn btn-danger">DELETE</button>
+                                </form>
+                            </td>
+                        </tr>
+                        <?php $i++?>
                     </div>
-                <?php endwhile ?>
-                <?php $conn->close() ?>
-            <?php endif ?>    
+                <?php endwhile;?>
+            </table>
         </div>
     </div>
 </body>
